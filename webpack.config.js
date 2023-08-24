2d9432bbd6f3c9e90b79364d18305d94bb9500b3
MiniOcean404 <1037306928@qq.com>
2023-08-25 03:58:36 +0800

    feat: 修改webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,11 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
-    // library: {
-    //   name: 'MyLibrary',
-    //   type: 'umd',
-    //   umdNamedDefine: true,
-    // },
+    library: {
+      name: 'mtEncrypt',
+      type: 'umd',
+      umdNamedDefine: true,
+    },
   },
   optimization: {
     minimize: false, // 打包是否压缩代码
